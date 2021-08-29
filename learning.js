cef0942f232f937b9b8a359037e0756562029bed
YamEbgui <yam.ebgui@gmail.com>
2021-08-29 18:04:12 +0300

    task number 2
    add a function that solve quadratic equation

--- learning.js
+++ learning.js
@@ -6,3 +6,15 @@ function sumThereDigitNumbers(nu){
 }
 sumThereDigitNumbers(num)
 console.log(sum);
+
+const a = 1; b = -1; c = 1;  // stands for x**2 - 6*x + 9 = 0
+let solutionA = solutionB = 0
+// your code goes here
+function solve(){
+    let newB=-b;
+    solutionA=(newB+Math.sqrt((newB*newB)-(4*a*c)))/(2*a);
+    solutionB=(newB-Math.sqrt((newB*newB)-(4*a*c)))/(2*a);
+
+}
+solve();
+console.log(solutionA, solutionB)
